782c8cd858e7c7ce0e06bc308901665bbd1e6db7
Gabriel Matozinho <gabriel.intec@hotmail.com>
2024-08-12 20:18:55 -0300

    Descreva as alterações feitas, como 'Adiciona projeto do Dia 03'

--- Day_03.py
+++ Day_03.py
@@ -1,27 +1,29 @@
 price = 0.0
-print("Welcome to Python Pizza Deliveries")
-size = input("What size pizza do you want [S, M, L]: ").upper()
 
+size = input("What size do you want [S,M or L]: ").upper()
 if size == 'S':
-    price += 15
-    pepperoni = input("Add pepperoni for small pizza [Y or N]: ").upper()
+    price += 15.0
+    pepperoni = input("Add pepperoni in small pizza [Y oir N]: ").upper()
     if pepperoni == 'Y':
-        price += 2
+        price += 2.0    
 
 elif size == 'M':
-    price += 20
-    pepperoni = input("Add pepperoni for medium pizza [Y or N]: ").upper()
+    price += 20.0
+    pepperoni = input("Add pepperoni in medium pizza [Y oir N]: ").upper()
     if pepperoni == 'Y':
-        price += 3
+        price += 3 
 
-elif size == 'L':
+if size == 'L':
     price += 25
-    pepperoni = input("Add pepperoni for large pizza [Y or N]: ").upper()
+    pepperoni = input("Add pepperoni in large pizza [Y oir N]: ").upper()
     if pepperoni == 'Y':
-        price += 3
+        price += 3    
 
-extra_cheese = input("Do you want extra cheese [Y or N]: ").upper()
+extra_cheese = input("Add extra cheese for any size pizza[Y or N]: ").upper()
 if extra_cheese == 'Y':
     price += 1
 
-print(f"Your final bill is: ${price:.2f}")
+print(f'Bill = {price:.2f}')
+
+
+    
